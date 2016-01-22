5bd22518ba0bccf09d06d14d4c055de799474898
DanyloKiral <kiral.danylo@gmail.com>
2016-01-23 00:06:02 +0200

    Routing replaced

--- PandaDemo/www/scripts/Controllers/LoadPageConroller.js
+++ PandaDemo/www/scripts/Controllers/LoadPageConroller.js
@@ -1,19 +1,4 @@
-﻿var myApp = angular.module('myApp', ['ngCordova', 'ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('loading');
-        $stateProvider.state('loading', {
-            url: '/loading',
-            templateUrl: '/loading.html',
-            controller: 'loadPageController'
-        })
-        .state('menu', {
-            url: '/menu',
-            templateUrl: '/menu.html',
-            controller: 'menuController'
-        })
-    });
-
-myApp.controller('loadPageController', function ($scope, $cordovaDialogs, $state) {
+﻿myApp.controller('loadPageController', function ($scope, $cordovaDialogs, $state) {
 
     var timeout = 3000; 
     var menuHref = 'menu';
--- PandaDemo/www/scripts/app.js
+++ PandaDemo/www/scripts/app.js
@@ -1,1 +1,14 @@
-﻿
+﻿var myApp = angular.module('myApp', ['ngCordova', 'ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('loading');
+        $stateProvider.state('loading', {
+            url: '/loading',
+            templateUrl: '/loading.html',
+            controller: 'loadPageController'
+        })
+        .state('menu', {
+            url: '/menu',
+            templateUrl: '/menu.html',
+            controller: 'menuController'
+        })
+    });
