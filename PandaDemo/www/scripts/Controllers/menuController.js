fcccbf0354cc58804f169f080cecdec7b5283955
DanyloKiral <kiral.danylo@gmail.com>
2016-01-23 00:07:58 +0200

    Added changing orientation detection

--- PandaDemo/www/scripts/Controllers/menuController.js
+++ PandaDemo/www/scripts/Controllers/menuController.js
@@ -4,4 +4,12 @@
         document.getElementById('pandaPng').style.width= 0.2 * (401/425) * screen.width + "px";
         document.getElementById('pandaPng').style.height = 0.2 * screen.width + "px";
     };
+
+    window.onorientationchange = function () {
+        if ((window.orientation == 0) || window.orientation == 180) {
+            
+        } else if (window.orientation == -90 || window.orientation == 90) {
+            
+        }
+    };
 });
