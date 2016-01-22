483f0141d8bca2834bf636457966c399267bb084
Natalie95 <Tifosaaaa@gmail.com>
2016-01-23 00:02:17 +0200

    width height script

--- PandaDemo/www/scripts/Controllers/menuController.js
+++ PandaDemo/www/scripts/Controllers/menuController.js
@@ -1,20 +1,7 @@
 ﻿myApp.controller('menuController', function ($scope, $cordovaDialogs, $cordovaDeviceOrientation) {
     $scope.load = function () {
-        var options = {
-            frequency: 3000,
-            filter: true     // if frequency is set, filter is ignored
-        }
-
-        var watch = $cordovaDeviceOrientation.watchHeading(options).then(
-          null,
-          function (error) {
-              // An error occurred
-          },
-          function (result) {   // updates constantly (depending on frequency value)
-              $cordovaDialogs.alert(result.magneticHeading);
-              var trueHeading = result.trueHeading;
-              var accuracy = result.headingAccuracy;
-              var timeStamp = result.timestamp;
-          });
+        var el = document.getElementById('pandaPng');
+        document.getElementById('pandaPng').style.width= 0.2 * (401/425) * screen.width + "px";
+        document.getElementById('pandaPng').style.height = 0.2 * screen.width + "px";
     };
 });
