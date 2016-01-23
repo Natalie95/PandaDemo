65ec60f21a83a295b7676bfa289ef543d96c3cbf
Natalie95 <Tifosaaaa@gmail.com>
2016-01-23 15:43:59 +0200

    chairs added, wardrobe positioned

--- PandaDemo/www/scripts/Controllers/LoadPageConroller.js
+++ PandaDemo/www/scripts/Controllers/LoadPageConroller.js
@@ -1,11 +1,13 @@
 ﻿myApp.controller('loadPageController', function ($scope, $cordovaDialogs, $state) {
 
-    var timeout = 3000; 
+    var timeout = 0000; 
     var menuHref = 'menu';
 
     $scope.loading = function () {
+        document.getElementById('panda-logo').style.width = 0.5 * (294 / 272) * screen.width + "px";
+        document.getElementById('panda-logo').style.height = 0.5 *  screen.width + "px";
         setTimeout(function () {
-            $state.go(menuHref);
+           $state.go(menuHref);
         }, timeout);
     };
 
--- PandaDemo/www/scripts/Controllers/menuController.js
+++ PandaDemo/www/scripts/Controllers/menuController.js
@@ -1,9 +1,24 @@
 ﻿myApp.controller('menuController', function ($scope, $cordovaDialogs, $cordovaDeviceOrientation) {
+
+
+    // panda-logo - panda on the loading page
+    // PandaPng - panda near the wardrobe (menu page)
+    // wardrobe (menu page)
+
+
     $scope.load = function () {
-        var el = document.getElementById('pandaPng');
-        document.getElementById('pandaPng').style.width= 0.2 * (401/425) * screen.width + "px";
-        document.getElementById('pandaPng').style.height = 0.2 * screen.width + "px";
+        document.getElementById('pandaPng').style.width= 0.23 * (401/425) * screen.width + "px";
+        document.getElementById('pandaPng').style.height = 0.23 * screen.width + "px";
+        if (screen.width < 768) {
+            document.getElementById('wardrobe').style.width = 0.8 * (924 / 943) * screen.width + "px";
+        } else {
+            document.getElementById('wardrobe').style.width = 0.6 * (924 / 943) * screen.width + "px";
+        }
+        document.getElementById('wardrobe').style.height = 0.98 * screen.height + "px";
+        document.getElementById('table').style.width = 0.2 * (551 / 305) * screen.width + "px";
+        document.getElementById('table').style.height = 0.2 * screen.width + "px";
     };
+
 
     window.onorientationchange = function () {
         if ((window.orientation == 0) || window.orientation == 180) {
