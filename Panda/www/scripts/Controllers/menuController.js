e9e50126f933ef185809fd1a887dbf88244d8821
DanyloKiral <kiral.danylo@gmail.com>
2016-01-25 20:04:59 +0200

    Buildable project

---
+++ Panda/www/scripts/Controllers/menuController.js
@@ -0,0 +1,42 @@
+﻿myApp.controller('menuController', function ($scope, $cordovaDialogs, $cordovaDeviceOrientation) {
+
+
+    // panda-logo - panda on the loading page
+    // PandaPng - panda near the wardrobe (menu page)
+    // wardrobe (menu page)
+    $scope.load = function () {
+        adapt();
+    };
+
+    window.onorientationchange = function () {
+        adapt();
+    };
+
+    var adapt = function () {
+        if ((window.orientation == 0) || window.orientation == 180) {
+             adaptiveSizeHorizontal();
+        } else if (window.orientation == -90 || window.orientation == 90) {
+             adaptiveSizeVertical();
+        }
+    };
+
+    var adaptiveSizeHorizontal = function () {
+        document.getElementById('pandaPng').style.width = 0.23 * (401 / 425) * screen.width + "px";
+        document.getElementById('pandaPng').style.height = 0.23 * screen.width + "px";
+        if (screen.width < 768) {
+            document.getElementById('wardrobe').style.width = 0.8 * (924 / 943) * screen.width + "px";
+        } else {
+            document.getElementById('wardrobe').style.width = 0.6 * (924 / 943) * screen.width + "px";
+        }
+        document.getElementById('wardrobe').style.height = 0.98 * screen.height + "px";
+        document.getElementById('table').style.width = 0.55 * (554 / 847) * screen.width + "px";
+        document.getElementById('table').style.height = 0.55 * screen.width + "px";
+        document.getElementById('clock').style.width = 0.2 * (344 / 343) * screen.width + "px";
+        document.getElementById('clock').style.height = 0.2 * screen.width + "px";
+    };
+
+    var adaptiveSizeVertical = function () {
+        
+    };
+
+});
---
+++ Panda/www/scripts/app.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.module('myApp', ['ngCordova', 'ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('loading');
+        $stateProvider.state('loading', {
+            url: '/loading',
+            templateUrl: 'partial/loading.html',
+            controller: 'loadPageController'
+        })
+        .state('menu', {
+            url: '/menu',
+            templateUrl: 'partial/menu.html',
+            controller: 'menuController'
+        })
+    });
